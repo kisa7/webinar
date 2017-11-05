ba131b4f33650a409b03844354810d60918d65a4
kisa7 <nastya.shev9594@gmail.com>
2017-11-05 13:40:39 +0300

    Ready to be deployed

---
+++ js/index-page.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    
+
+    
+    $('.slider-wr-sec-2').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        nextArrow: '<button type="button" class="slick-next"><span>002</span></button>',
+        responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 1,
+                
+              }
+            }
+          ]
+    });
+    
+   
+    
+    
+    
+    
+    
+});
